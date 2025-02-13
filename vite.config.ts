1eb84b9b0123ca47375b570d9d926d8f89fa5235
Cuyox3 <artuciiz@gmail.com>
2025-02-12 20:00:50 -0600

    -port

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,4 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  server: { // <-- Añade esta sección server
-    port: 80, // <-- Define el puerto aquí, por ejemplo 8080
-  },
 });
